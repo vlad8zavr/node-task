dd024d191b102ae355747e409f0d4af60b77ec85
vlad8zavr <47221693+vlad8zavr@users.noreply.github.com>
2019-09-13 16:41:49 +0300

    Delete input.js

--- test-my-server/input.js
+++
@@ -1,8 +0,0 @@
-
-let nodePath = process.argv[0];
-let appPath = process.argv[1];
-let firstRealArgument = process.argv[2];
-
-console.log(nodePath);
-console.log(appPath);
-console.log("firstRealArgument: " + firstRealArgument);
